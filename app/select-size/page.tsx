7495f91d44d89924e1a58102f3ca6f13cd50922d
Jasmin Raith <jasminraith@icloud.com>
2024-08-01 14:35:47 +0200

    add button to select-size page

--- app/select-size/page.tsx
+++ app/select-size/page.tsx
@@ -9,7 +9,7 @@ export default function SelectSize() {
 
       <img src="/body-measurements.png" alt="" className="" />
       
-      <div className="relative">
+      <div className="relative flex justify-between">
         <label
           htmlFor="name"
           className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
@@ -21,8 +21,15 @@ export default function SelectSize() {
           name="name"
           type="text"
           placeholder="36cm"
-          className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-5/12 rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
+
+        <button
+          type="button"
+          className="rounded-md w-5/12 bg-[var(--accent-color)] px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-[var(--accent-color-light)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--accent-color)]"
+        >
+          Weiter
+        </button>
       </div>
 
     </div>
